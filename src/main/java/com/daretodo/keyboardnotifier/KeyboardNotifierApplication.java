4923c261261670f917805217254f3033af79d184
juy4556 <wnstjrtmdgus@gmail.com>
2024-09-09 00:38:40 +0900

    init

---
+++ src/main/java/com/daretodo/keyboardnotifier/KeyboardNotifierApplication.java
@@ -0,0 +1,13 @@
+package com.daretodo.keyboardnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyboardNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyboardNotifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daretodo/keyboardnotifier/KeyboardNotifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daretodo.keyboardnotifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyboardNotifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
