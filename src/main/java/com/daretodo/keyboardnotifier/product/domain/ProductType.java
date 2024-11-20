f7c229a41a96d7a3cf4497c210d08b3993df6692
juy4556 <wnstjrtmdgus@gmail.com>
2024-11-20 18:03:53 +0900

    refactor: ProductType 내 koreanName 필드 추가

--- src/main/java/com/daretodo/keyboardnotifier/product/domain/ProductType.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/domain/ProductType.java
@@ -1,10 +1,17 @@
 package com.daretodo.keyboardnotifier.product.domain;
 
 public enum ProductType {
-    KIT,
-    KEYBOARD,
-    SWITCH,
-    KEY_CAP,
-    STABILIZER,
-    PARTS,
+    KIT("키트"),
+    KEYBOARD("키보드"),
+    SWITCH("스위치"),
+    KEYCAP("키캡"),
+    STABILIZER("스타빌라이저"),
+    PARTS("부품"),
+    ETC("기타");
+
+    private final String koreanName;
+
+    ProductType(String koreanName) {
+        this.koreanName = koreanName;
+    }
 }
