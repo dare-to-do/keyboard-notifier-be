3f2d572f24c248f8f3d77d38a3e94c920a80cd9e
juy4556 <wnstjrtmdgus@gmail.com>
2024-11-20 18:02:47 +0900

    refactor: ProductStatus value 필드 제거

--- src/main/java/com/daretodo/keyboardnotifier/product/domain/ProductStatus.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/domain/ProductStatus.java
@@ -4,15 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum ProductStatus {
-    NOT_YET("예정", "NOT YET"),
-    IN_PROGRESS("진행중", "IN PROGRESS"),
-    DONE("종료", "DONE");
+    NOT_YET("예정"),
+    IN_PROGRESS("진행중"),
+    DONE("종료"),
+    UNKNOWN("알수없음");
 
     private final String description;
-    private final String value;
 
-    ProductStatus(String description, String value) {
+    ProductStatus(String description) {
         this.description = description;
-        this.value = value;
     }
 }
