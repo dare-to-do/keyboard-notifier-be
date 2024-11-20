56ac58fb7f23ef26a3c1852bc90c271ab8cabdfc
juy4556 <wnstjrtmdgus@gmail.com>
2024-11-20 16:45:25 +0900

    refactor: Product 관련 요청 전부 '/products'로 시작

--- src/main/java/com/daretodo/keyboardnotifier/product/controller/ProductController.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/controller/ProductController.java
@@ -13,20 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
     private final ProductService productService;
 
     @Operation(summary = "상품 다건 등록")
-    @PostMapping("/products")
-    public GiBiResponseBody<Integer> createProduct(@RequestBody ProductCreateRequest requests) {
+    @PostMapping
+    public SokeyResponseBody<Integer> createProduct(@RequestBody ProductCreateRequest requests) {
         var createdCount = productService.createProducts(requests.toProducts());
         return GiBiResponseBody.success(createdCount);
     }
 
     @Operation(summary = "상품 전체 조회")
-    @GetMapping("/products")
-    public GiBiResponseBody<PageableOutput<ProductResponse>> findAllProducts(ProductsRequestCondition condition) {
+    @GetMapping
+    public SokeyResponseBody<PageableOutput<ProductResponse>> findAllProducts(ProductsRequestCondition condition) {
         var products = productService.findAllProducts(condition.getProductStatus(), condition.getProductType(), condition.getPageable(), condition.getSortBy());
         return GiBiResponseBody.success(new PageableOutput<>(products));
     }
