ccb16bdca280ddd582ce84ddc44f7cdd197927a2
juy4556 <wnstjrtmdgus@gmail.com>
2024-12-10 18:29:56 +0900

    refactor: 특정 origin만 cors 허용하도록 변경

--- src/main/java/com/daretodo/keyboardnotifier/config/WebConfig.java
+++ src/main/java/com/daretodo/keyboardnotifier/config/WebConfig.java
@@ -17,7 +17,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("*")
+                .allowedOrigins(parseCorsList(corsList))
                 .allowedHeaders("*")
                 .allowedMethods("*")
                 .allowCredentials(true);
