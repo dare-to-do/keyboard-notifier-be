c18a885be881fbdb5dc605019ef167688ccfb964
juy4556 <wnstjrtmdgus@gmail.com>
2024-12-22 16:57:10 +0900

    feature: ProductEventListener 추가

---
+++ src/main/java/com/daretodo/keyboardnotifier/product/application/ProductEventListener.java
@@ -0,0 +1,27 @@
+package com.daretodo.keyboardnotifier.product.application;
+
+import com.daretodo.keyboardnotifier.product.domain.ProductEvent;
+import com.daretodo.keyboardnotifier.product.domain.ProductEvent.EventType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductEventListener {
+    private final ViewCountService viewCountService;
+
+    @EventListener
+    public void handleProductEvent(ProductEvent event) {
+        handleProductReadEvent(event);
+    }
+
+    private void handleProductReadEvent(ProductEvent event) {
+        if (!IsReadEvent(event)) return;
+        viewCountService.increaseViewCount(event.getProduct());
+    }
+
+    private boolean IsReadEvent(ProductEvent event) {
+        return event.getEventType() == EventType.READ;
+    }
+}
