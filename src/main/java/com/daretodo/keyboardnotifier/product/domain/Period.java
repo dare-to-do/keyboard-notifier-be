503fce709281fea6bc68d10018cc0b7365f7850e
juy4556 <wnstjrtmdgus@gmail.com>
2024-12-22 16:33:53 +0900

    refactor: Period record 클래스 -> 일반 클래스로 리팩토링

--- src/main/java/com/daretodo/keyboardnotifier/product/domain/Period.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/domain/Period.java
@@ -1,11 +1,20 @@
 package com.daretodo.keyboardnotifier.product.domain;
 
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
-public record Period(
-    LocalDateTime startDate,
-    LocalDateTime endDate
-) {
+@Getter
+@Embeddable
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Period {
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
 
     public static Period of(LocalDateTime startDate, LocalDateTime endDate) {
         return new Period(startDate, endDate);
