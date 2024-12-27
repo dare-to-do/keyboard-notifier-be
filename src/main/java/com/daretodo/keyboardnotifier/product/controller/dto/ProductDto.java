7b25b49447f9419c11a8335e9f91772c66ed91b6
juy4556 <wnstjrtmdgus@gmail.com>
2024-12-27 20:22:38 +0900

    refactor: ProductDto -> Product 시 builder 패턴 적용하여 가독성 향상

--- src/main/java/com/daretodo/keyboardnotifier/product/controller/dto/ProductDto.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/controller/dto/ProductDto.java
@@ -33,23 +33,18 @@ public record ProductDto(
         Assert.hasText(productUrl, "상품 URL은 필수입니다.");
         Assert.notNull(productType, "상품 종류는 필수입니다.");
 
-        return new Product(
-                null,
-                name,
-                price,
-                PriceUnit.from(unit),
-                imageUrls,
-                productUrl,
-                ProductType.from(productType),
-                description,
-                Period.of(startDate, endDate),
-                getProductStatus(startDate, endDate),
-                null,
-                null,
-                null,
-                null,
-                null
-        );
+        return Product.builder()
+                .id(null)
+                .name(name)
+                .price(price)
+                .priceUnit(PriceUnit.from(unit))
+                .imageUrl(imageUrls)
+                .productUrl(productUrl)
+                .type(ProductType.from(productType))
+                .description(description)
+                .period(Period.of(startDate, endDate))
+                .status(getProductStatus(startDate, endDate))
+                .build();
     }
 
     private ProductStatus getProductStatus(LocalDateTime startDate, LocalDateTime endDate) {
