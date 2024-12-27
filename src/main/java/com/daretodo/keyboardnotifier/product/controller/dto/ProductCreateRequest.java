05f1da4e40efc5c5c8818265a1e56ea866f0ffce
juy4556 <wnstjrtmdgus@gmail.com>
2024-12-27 20:24:27 +0900

    chore: 불필요한 import 제거

--- src/main/java/com/daretodo/keyboardnotifier/product/controller/dto/ProductCreateRequest.java
+++ src/main/java/com/daretodo/keyboardnotifier/product/controller/dto/ProductCreateRequest.java
@@ -1,6 +1,5 @@
 package com.daretodo.keyboardnotifier.product.controller.dto;
 
-import com.daretodo.keyboardnotifier.product.controller.ProductDto;
 import com.daretodo.keyboardnotifier.product.domain.Product;
 import io.swagger.v3.oas.annotations.media.Schema;
 
