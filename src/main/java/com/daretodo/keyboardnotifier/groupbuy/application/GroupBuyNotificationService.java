0dec419d2c100d9255c38331eb80fdf75f2a7dcf
juy4556 <wnstjrtmdgus@gmail.com>
2025-02-14 00:04:25 +0900

    feature: groupBuyEmailContent 메서드 static으로 변경

--- src/main/java/com/daretodo/keyboardnotifier/groupbuy/application/GroupBuyNotificationService.java
+++ src/main/java/com/daretodo/keyboardnotifier/groupbuy/application/GroupBuyNotificationService.java
@@ -23,7 +23,6 @@ import static com.daretodo.keyboardnotifier.common.util.DateUtil.formatDateWithT
 @Slf4j
 public class GroupBuyNotificationService {
     private final EmailSendService emailSendService;
-    private final GroupBuyEmailContentGenerator groupBuyEmailContentGenerator;
     private final GroupBuyNotificationRepository groupBuyNotificationRepository;
     private final GroupBuyParticipantRepository groupBuyParticipantRepository;
     private final UserRepository userRepository;
@@ -36,8 +35,8 @@ public class GroupBuyNotificationService {
     private String feedbackFormLink;
 
     public void sendGroupBuyStartNotification(List<GroupBuy> groupBuys) {
-        for (GroupBuy groupBuy : groupBuys) {
-            List<GroupBuyParticipant> participants = groupBuy.getParticipants();
+        for (GroupBuy startGroupBuy : groupBuys) {
+            List<GroupBuyParticipant> participants = startGroupBuy.getParticipants();
 
             for (GroupBuyParticipant participant : participants) {
                 if (participant.getStatus() != GroupBuyParticipantStatus.PARTICIPATED) {
@@ -45,38 +44,66 @@ public class GroupBuyNotificationService {
                 }
 
                 User user = userRepository.findById(participant.getUserId());
-                Product product = productRepository.findById(groupBuy.getProductId());
+                Product product = productRepository.findById(startGroupBuy.getProductId());
 
-                String title = groupBuyEmailContentGenerator.generateStartTitle(product.getName());
-                String content = groupBuyEmailContentGenerator.generateStartContent(
+                String title = GroupBuyEmailContentGenerator.generateStartTitle(product.getName());
+                String content = GroupBuyEmailContentGenerator.generateStartContent(
                         product.getName(),
-                        formatDateWithTime(groupBuy.getEndDateTime()),
+                        formatDateWithTime(startGroupBuy.getEndDateTime()),
                         product.getProductUrl(),
                         feedbackFormLink
                 );
 
                 Email email = Email.of(senderEmail, user.getEmail(), title, content);
-
-                SendEmailRequest sendEmailRequest = email.buildEmailResult();
-                SendEmailResponse sendEmailResponse = emailSendService.sendEmail(sendEmailRequest);
-
-                if (sendEmailResponse != null && sendEmailResponse.messageId() != null) {
-                    groupBuyNotificationRepository.save(GroupBuyNotification.builder()
-                            .messageId(sendEmailResponse.messageId())
-                            .receiverId(user.getId())
-                            .groupBuyId(groupBuy.getId())
-                            .status(GroupBuyNotificationStatus.SENT)
-                            .build());
-                    groupBuyParticipantRepository.updateStatus(participant.getId(), GroupBuyParticipantStatus.COMPLETED);
-                    continue;
-                }
-                groupBuyNotificationRepository.save(GroupBuyNotification.builder()
-                        .messageId(null)
-                        .receiverId(user.getId())
-                        .groupBuyId(groupBuy.getId())
-                        .status(GroupBuyNotificationStatus.FAILED)
-                        .build());
+                sendEmail(startGroupBuy, participant, user.getId(), email);
             }
         }
+    }
+
+    public void sendGroupBuyEndNotification(List<GroupBuy> groupBuys) {
+        for (GroupBuy endGroupBuy : groupBuys) {
+            List<GroupBuyParticipant> participants = endGroupBuy.getParticipants();
+
+            for (GroupBuyParticipant participant : participants) {
+                if (participant.getStatus() != GroupBuyParticipantStatus.PARTICIPATED) {
+                    continue;
+                }
+
+                User user = userRepository.findById(participant.getUserId());
+                Product product = productRepository.findById(endGroupBuy.getProductId());
+
+                String title = GroupBuyEmailContentGenerator.generateEndTitle(product.getName());
+                String content = GroupBuyEmailContentGenerator.generateEndContent(
+                        product.getName(),
+                        product.getProductUrl(),
+                        feedbackFormLink
+                );
+
+                Email email = Email.of(senderEmail, user.getEmail(), title, content);
+                sendEmail(endGroupBuy, participant, user.getId(), email);
+            }
+        }
+    }
+
+    private void sendEmail(GroupBuy groupBuy, GroupBuyParticipant participant, Long userId, Email email) {
+        SendEmailRequest sendEmailRequest = email.buildEmailResult();
+        SendEmailResponse sendEmailResponse = emailSendService.sendEmail(sendEmailRequest);
+
+        if (sendEmailResponse != null && sendEmailResponse.messageId() != null) {
+            groupBuyNotificationRepository.save(GroupBuyNotification.builder()
+                    .messageId(sendEmailResponse.messageId())
+                    .receiverId(userId)
+                    .groupBuyId(groupBuy.getId())
+                    .status(GroupBuyNotificationStatus.SENT)
+                    .build());
+            groupBuyParticipantRepository.updateStatus(participant.getId(), GroupBuyParticipantStatus.COMPLETED);
+            return;
+        }
+        groupBuyNotificationRepository.save(GroupBuyNotification.builder()
+                .messageId(null)
+                .receiverId(userId)
+                .groupBuyId(groupBuy.getId())
+                .status(GroupBuyNotificationStatus.FAILED)
+                .build());
     }
 }
