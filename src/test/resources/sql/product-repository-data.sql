a79b619e36877b220f1b305af1235715a102194b
juy4556 <wnstjrtmdgus@gmail.com>
2025-02-15 18:55:18 +0900

    refactor: 테스트 product 데이터 리팩토링

--- src/test/resources/sql/product-repository-data.sql
+++ src/test/resources/sql/product-repository-data.sql
@@ -5,21 +5,21 @@ VALUES (1, NOW(), 'admin', NOW(), 'admin', 'Product A', 1000, 'KRW', 'http://exa
         'Description for Product A', '2024-08-01', '2024-08-31', 'IN_PROGRESS', 0);
 INSERT INTO product (id, created_at, created_by, updated_at, updated_by, name, price, price_unit, image_url,
                      product_url, type, description, start_date, end_date, status)
-VALUES (2, NOW() + 1, 'admin', NOW(), 'admin', 'Product B', 2000, 'USD', 'http://example.com/imageB.jpg',
+VALUES (2, NOW() + 1, 'admin', NOW(), 'admin', 'Product B', 2000, 'KRW', 'http://example.com/imageB.jpg',
         'http://example.com/productB', 'SWITCH',
-        'Description for Product B', '2024-08-01', '2024-08-31', 'NOT_YET');
+        'Description for Product B', '2024-08-01', '2024-08-31', 'PENDING');
 INSERT INTO product (id, created_at, created_by, updated_at, updated_by, name, price, price_unit, image_url,
                      product_url, type, description, start_date, end_date, status)
-VALUES (3, NOW() + 2, 'admin', NOW(), 'admin', 'Product C', 1500, 'EUR', 'http://example.com/imageC.jpg',
+VALUES (3, NOW() + 2, 'admin', NOW(), 'admin', 'Product C', 1500, 'KRW', 'http://example.com/imageC.jpg',
         'http://example.com/productC', 'KEYCAP',
-        'Description for Product C', '2024-08-01', '2024-08-31', 'DONE');
+        'Description for Product C', '2024-08-01', '2024-08-31', 'COMPLETED');
 INSERT INTO product (id, created_at, created_by, updated_at, updated_by, name, price, price_unit, image_url,
                      product_url, type, description, start_date, end_date, status)
-VALUES (4, NOW() + 3, 'admin', NOW(), 'admin', 'Product D', 2500, 'JPY', 'http://example.com/imageD.jpg',
+VALUES (4, NOW() + 3, 'admin', NOW(), 'admin', 'Product D', 2500, 'KRW', 'http://example.com/imageD.jpg',
         'http://example.com/productD', 'STABILIZER',
         'Description for Product D', '2024-08-01', '2024-08-31', 'IN_PROGRESS');
 INSERT INTO product (id, created_at, created_by, updated_at, updated_by, name, price, price_unit, image_url,
                      product_url, type, description, start_date, end_date, status)
-VALUES (5, NOW() + 4, 'admin', NOW(), 'admin', 'Product E', 3000, 'CNY', 'http://example.com/imageE.jpg',
+VALUES (5, NOW() + 4, 'admin', NOW(), 'admin', 'Product E', 3000, 'KRW', 'http://example.com/imageE.jpg',
         'http://example.com/productE', 'KIT',
-        'Description for Product E', '2024-08-01', '2024-08-31', 'NOT_YET');
+        'Description for Product E', '2024-08-01', '2024-08-31', 'PENDING');
