03e3ccf6175cc2292e93afa588993ac612f5fed0
juy4556 <wnstjrtmdgus@gmail.com>
2025-02-23 22:53:14 +0900

    refactor: 공제시작 메세지 변경

--- src/main/java/com/daretodo/keyboardnotifier/groupbuy/application/GroupBuyEmailContentGenerator.java
+++ src/main/java/com/daretodo/keyboardnotifier/groupbuy/application/GroupBuyEmailContentGenerator.java
@@ -9,19 +9,32 @@ public class GroupBuyEmailContentGenerator {
     }
 
     public static String generateStartContent(String productName, String endDate, String productLink, String feedbackFormLink) {
+        String scheduleMessage = generateScheduleMessage(productName, endDate);
+
         return String.format(
-                "<html>" +
-                        "<body style='font-family: Arial, sans-serif; line-height: 1.6; color: #333;'>" +
-                        "<p>안녕하세요, <b>Sokey</b>에서 공제 일정 소식 알려드립니다.</p>" +
-                        "<p>관심상품으로 등록하신 <b>%s</b> 공제가 오늘부터 <b>%s</b>까지 진행 될 예정입니다.</p>" +
-                        "<p>관련 상세 정보는 아래 링크를 통해 확인하세요.</p>" +
-                        "<p><a href='%s' style='color: #0066cc; text-decoration: none;'><b>%s</b></a></p>" +
-                        "<p>Sokey 서비스를 이용하시면서 불편하셨던 점이나 개선 사항이 있다면 문의 남겨주세요.</p>" +
-                        "<p><a href='%s' style='color: #0066cc; text-decoration: none;'><b>%s</b></a></p>" +
-                        "</body>" +
-                        "</html>",
-                productName, endDate, productLink, productLink, feedbackFormLink, feedbackFormLink
+        "<html>" +
+        "<body style='font-family: Arial, sans-serif; line-height: 1.6; color: #333;'>" +
+            "<p>안녕하세요, <b>Sokey</b>에서 공제 일정 소식 알려드립니다.</p>" +
+            "%s" +
+            "<p>관련 상세 정보는 아래 링크를 통해 확인하세요.</p>" +
+            "<p><a href='%s' style='color: #0066cc; text-decoration: none;'><b>%s</b></a></p>" +
+            "<p>Sokey 서비스를 이용하시면서 불편하셨던 점이나 개선 사항이 있다면 문의 남겨주세요.</p>" +
+            "<p><a href='%s' style='color: #0066cc; text-decoration: none;'><b>%s</b></a></p>" +
+        "</body>" +
+        "</html>",
+        scheduleMessage, productLink, productLink, feedbackFormLink, feedbackFormLink
         );
+    }
+        
+    private static String generateScheduleMessage(String productName, String endDate) {
+        if (endDate == null || endDate.isEmpty()) {
+        return String.format(
+        "<p>관심상품으로 등록하신 <b>%s</b> 공제가 오늘부터 진행 될 예정입니다.</p>",
+        productName);
+        } 
+        return String.format(
+        "<p>관심상품으로 등록하신 <b>%s</b> 공제가 오늘부터 <b>%s</b>까지 진행 될 예정입니다.</p>",
+        productName, endDate);
     }
 
     public static String generateEndTitle(String productName) {
